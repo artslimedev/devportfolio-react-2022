79c00d417434b43c629af8df0ec0f38ca19537a3
artslimedev <102567983+artslimedev@users.noreply.github.com>
2022-12-03 13:38:02 -0500

    deploying my new portfolio site

--- src/ components/Navbar.jsx
+++ src/ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const [nav, setNave] = useState(false)
     const handleClick = () => setNave(!nav)
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-white'>
+    <div className='fixed text-1xl w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-white'>
         <div>
             <img src={Logo} alt="Logo" style={{width: '70px'}} />
         </div>
