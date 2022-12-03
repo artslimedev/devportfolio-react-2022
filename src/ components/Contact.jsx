503d62217db2e246bc22de0e91a85f629c742a07
artslimedev <102567983+artslimedev@users.noreply.github.com>
2022-12-03 18:07:59 -0500

    Optimization

--- src/ components/Contact.jsx
+++ src/ components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form netlify name='contact' action='https://getform.io/f/bd36a41b-ca45-4488-8621-6c59dc893a46' method='POST' className='flex flex-col max-w-[600px] w-full'>
+        <form netlify action='https://getform.io/f/bd36a41b-ca45-4488-8621-6c59dc893a46' method='POST' className='flex flex-col max-w-[600px] w-full'>
             <div className="pb-8">
                 <p className='text-4xl font-bold inline border-b-4 border-blue-400 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form or shoot me an email - <span className='hover:border-b-2 hover:border-[#47b2ff] hover:pb-2'><a href='mailto:artslimedev@gmail.com'>artslimedev@gmail.com</a></span></p>
