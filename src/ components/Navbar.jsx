459052ceb6662a045cb527a32b6e8aefb89691ce
artslimedev <102567983+artslimedev@users.noreply.github.com>
2023-01-06 20:34:20 -0500

    updated links

--- src/ components/Navbar.jsx
+++ src/ components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                     <a className='flex justify-between items-center w-full text-white'
-                     href="https://drive.google.com/file/d/1a1PH-Cvpnos-muAJTaE1wLYW1Ko1O3AA/view?usp=sharing">
+                     href="https://drive.google.com/file/d/1K9t0mDaRipvodVqAhMLAqnGMbBMNDZa7/view?usp=sharing">
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
