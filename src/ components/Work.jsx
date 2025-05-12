baae7565185b135ab03934d0c75f879aafc88f52
artslimedev <102567983+artslimedev@users.noreply.github.com>
2025-05-12 13:56:11 -0400

    Pushing to add project to work

--- src/ components/Work.jsx
+++ src/ components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PhotogSite from "../assets/projects/photogsite.png";
 import PadFinder from "../assets/projects/padfinder.png";
 import Tempest from "../assets/projects/tempest.jpeg";
+import LockIn from "../assets/projects/LockinTasks.png";
 
 const Work = () => {
   return (
@@ -40,19 +41,19 @@ const Work = () => {
           </div>
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${PadFinder})` }}
+            style={{ backgroundImage: `url(${LockIn})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-1xl font-bold text-white tracking-wider">
-                PadFinder Marketplace
+                Lock In Task Manager
               </span>
               <div className="pt-8 text-center">
                 {/* <a href="https://padfindermarketplace.netlify.app/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a> */}
-                <a href="https://github.com/artslimedev/react-padfinder-website">
+                <a href="https://github.com/artslimedev/lockin-taskmanager">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
